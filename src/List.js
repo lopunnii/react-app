5f6f83c27fa35540253822ef967bf7121ee89cc0
lopunnii <87401100+lopunnii@users.noreply.github.com>
2023-04-13 17:01:23 +0900

    これから追加する機能のhtml追加

--- src/List.js
+++ src/List.js
@@ -5,17 +5,23 @@ function Item({ name }) {
   function handleClick() {
     setIsComplete(!isComplete);
 }
-    return <li onClick={handleClick}>{isComplete ? (<del>{name + ' ✔'}</del>) : name}</li>;
+    return <li onClick={handleClick}>{isComplete ? (<del>{name}</del>) : name}</li>;
 }
 
 export default function List() {
 return (
+<div>
+<h4>Tasks Completed: 0</h4>
+<h4>Add Task:</h4>
+<input type="text"></input>
+<button>+</button>
 <div className="list-box">
 <ul className="list">
   <Item name="Do Yoga"></Item>
   <Item name="Cook Curry"></Item>
   <Item name="Study React"></Item>
 </ul>
+</div>
 </div>
 );
 }
