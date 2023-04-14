188084ea6daeddeda3676084465620d43151dfe1
lopunnii <87401100+lopunnii@users.noreply.github.com>
2023-04-14 11:01:29 +0900

    タスクをliからチェックボックスに変更

--- src/List.js
+++ src/List.js
@@ -5,22 +5,36 @@ function Item({ name }) {
   function handleClick() {
     setIsComplete(!isComplete);
 }
-    return <li onClick={handleClick}>{isComplete ? (<del>{name}</del>) : name}</li>;
+    return (
+    <label className="task">
+      <input type="checkbox" onClick={handleClick} checked={isComplete}></input>
+      {isComplete ? (<del>{name}</del>) : name}
+    </label>
+    )
+
 }
 
-export default function List() {
+export default function ToDoListApp() {
 return (
 <div>
+{/* タイトル */}
+<h1>To Do List</h1>
+
+{/* 完了したタスク数 */}
 <h4>Tasks Completed: 0</h4>
+
+{/* タスクを追加 */}
 <h4>Add Task:</h4>
 <input type="text"></input>
 <button>+</button>
+
+{/* タスクリスト */}
 <div className="list-box">
-<ul className="list">
+<div className="list">
   <Item name="Do Yoga"></Item>
   <Item name="Cook Curry"></Item>
   <Item name="Study React"></Item>
-</ul>
+</div>
 </div>
 </div>
 );
