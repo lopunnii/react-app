574621580f514390559d62f1bfb8157acd57d0bf
lopunnii <87401100+lopunnii@users.noreply.github.com>
2023-04-14 11:57:36 +0900

    dfd

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
-export function TaskList({ tasks }) {
-    const [isComplete, setIsComplete] = useState(false);
-    function handleClick() {
-      setIsComplete(!isComplete);
-  }
-      return (
-      <label className="task">
-        <input type="checkbox" onClick={handleClick} checked={isComplete}></input>
-        {isComplete ? (<del>{tasks.text}</del>) : tasks.text}
-      </label>
-      )
+// export function TaskList({ tasks }) {
+//     const [isComplete, setIsComplete] = useState(false);
+//     function handleClick() {
+//       setIsComplete(!isComplete);
+//   }
+//       return (
+//       <label className="task">
+//         <input type="checkbox" onClick={handleClick} checked={isComplete}></input>
+//         {isComplete ? (<del>{tasks.text}</del>) : tasks.text}
+//       </label>
+//       )
   
+//   }
+
+  export function TaskList( {tasks} ) {
+    return (
+  <ul>
+  {tasks.map(task => (
+    <li key={task.id}>{task.text}</li>
+  ))}
+  </ul>
+    );
   }
